9ef87262ef81b44c07080375b1005dfc02c95997
dahamdevtools <dahamdevtools@gmail.com>
2026-09-22 20:19:52 +0530

    feat: create DashboardLayout component with user session display and navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,10 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={`${poppins.className} h-full antialiased`}>
+    <html
+      lang="en"
+      className={`${poppins.className} h-full antialiased bg-white`}
+    >
       <body className="min-h-full flex flex-col items-center overflow-x-hidden bg-neutral-200/50 text-neutral-950">
         {children}
       </body>
