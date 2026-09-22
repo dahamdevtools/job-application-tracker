3887595eb423d8afad1eeca0f56cb24e7290d9f7
dahamdevtools <dahamdevtools@gmail.com>
2026-09-22 19:10:52 +0530

    feat: enhance home page layout with title and description for job application tracking

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center p-7">
-      Home
+    <div className="w-screen h-screen flex flex-col items-center justify-center overflow-y-scroll p-7 gap-3.5">
+      <h1 className="text-3xl text-center font-semibold">
+        Job Application Tracker
+      </h1>
+      <h2 className="text-lg text-center text-neutral-500">
+        Track, manage, and stay on top of your job search.
+      </h2>
+      <Link
+        href={"/login"}
+        className="w-fit h-10 flex items-center justify-center rounded-xl px-5 text-indigo-500 bg-indigo-100"
+      >
+        Get Started
+      </Link>
     </div>
   );
 }
