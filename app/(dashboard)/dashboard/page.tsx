61a1a12bfcfd831e82d40bb0a5bdd3936192209f
dahamdevtools <dahamdevtools@gmail.com>
2026-09-22 20:22:36 +0530

    feat: add Applications and Dashboard components for application tracking

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="w-full min-h-0 min-w-0 flex flex-col gap-7 p-3.5 pt-7 overflow-y-auto">
+      <h1 className="text-xl">Dashboard</h1>
+    </div>
+  );
+}
